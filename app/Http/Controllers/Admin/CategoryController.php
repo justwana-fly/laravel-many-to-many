be8011e8db11606f39e5bc7c40fff86461842c9f
klelia <clelia.fradella@gmail.com>
2024-06-05 13:32:55 +0200

    fix cat dd

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -39,7 +39,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //dd($category->posts);
+        //dd($category);
         return view('admin.categories.show', compact('category'));
     }
 
